1ab3a7664c34d7cd049050f8c5fbd1a3a7b8c309
root <root@dadmin-workstation.dsab-eud-101>
2016-10-24 15:09:50 +0100

    Installs Kubernetes v2

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ swapon /dev/vg-workstation/swap
 apt-get install openjdk-8-jdk
 apt-get install openvpn
 
-export KUBERNETES_PROVIDER=aws; curl -sS https://get.k8s.io | bash
+curl -sS https://get.k8s.io | bash
 
 OS=$(cat /etc/os-release|sed -e 's/"//'|grep ID_LIKE|awk -F '=' '{print $2}'|awk '{print $1}')
 
